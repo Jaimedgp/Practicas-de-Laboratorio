eeb85c37b7163f9cebc227cd880464642e290e23
Jaime Díez González-Pardo <jaime.diez.gp@gmail.com>
2018-04-27 23:49:05 +0200

    Final Version

--- Astronomy/Pleiades/CalcScript.py
+++ Astronomy/Pleiades/CalcScript.py
@@ -1,7 +1,11 @@
 import sys
 sys.path.insert(0, '/home/jaimedgp/J_Graphics_P/') # some other scripts from other project are going to be used
 
-folderPath = '/home/jaimedgp/Dropbox/VerbTeX/Astronomy/Pleiades/' #folder path 
+# Use another script to save athe data in LaTex table format
+# SaveScript in: https://github.com/Jaimedgp/J_Graphics_P/SaveScript.py
+from SaveScript import saveLaTex
+
+folderPath = '/home/jaimedgp/Dropbox/Astronomy/Pleiades/' #folder path 
 
 fw = open(folderPath+"Data/results.txt", "w") #text file where the results are gonna save
 
@@ -31,11 +35,7 @@ def ObtainData():
     for i in range(len(OurData['$m_V$'])):
         OurData['$m_V$'][i] = float(OurData['$m_V$'][i])
 
-    # Use another script to save athe data in LaTex table format
-    # SaveScript in: https://github.com/Jaimedgp/J_Graphics_P/SaveScript.py
-    from SaveScript import saveLaTex
-
-    saveLaTex(OurData, Ourindex, [0,1,2], folderPath+'TexTables/data.tex') #save the data in Latex table format
+    saveLaTex(table, index, [0,2,3,5,6], folderPath+'TexTables/data.tex') #save the data in Latex table format
 
     return OurData, Ourindex
 
@@ -57,10 +57,12 @@ def Plot(data, index, axes, name, types):
     plt.xlabel(index[axes[0]], fontsize=20)
     plt.ylabel(index[axes[1]], fontsize=20)
     plt.xlim(-0.4, 1.8)
-    plt.show()
+    #plt.show()
 
     fig.set_size_inches(9,6)
     fig.savefig(folderPath+"Figures/"+name+".png")
+
+    fig.clear() # erase the plotted value
 
 def Plot2(data, index, axes, name):
     import matplotlib.pyplot as plt
@@ -84,9 +86,13 @@ def Plot2(data, index, axes, name):
             MV.append(MVth[i])
         i = i+1
 
+    MiddleData = {index[axes[0]]: BV, index[axes[1]]:MV}
+    Middleindex = {0:index[axes[0]], 1:index[axes[1]]}
+
     # Calculate the linear regression
     slope, intercept, r_value, p_value, error = stats.linregress(BV, MV)
 
+    #Calc of standar deviation of the intercept
     BVmean = np.mean(BV)
     sx2 = ((BV-BVmean)**2).sum()
     std_intercept = error * np.sqrt(1./len(BV) + BVmean*BVmean/sx2)
@@ -102,8 +108,8 @@ def Plot2(data, index, axes, name):
     x = np.linspace(min(BV), max(BV), 1000)
     plt.plot(x, slope*x+intercept, 'r')
 
-    ax1.set_xlabel(index[axes[0]], fontsize=20)
-    ax1.set_ylim(17, 0)
+    ax1.set_xlabel("$B-V$", fontsize=20)
+    ax1.set_ylim(17, -8)
     # Make the y-axis label, ticks and tick labels match the line color.
     ax1.set_ylabel(index[axes[1]], color='r', fontsize=20)
     ax1.set_xlim(0.2, 1.6)
@@ -127,10 +133,30 @@ def Plot2(data, index, axes, name):
                 mV.append(mVexp[i])
         i = i+1
 
-    # Calculate the linear regression
-    slope, intercept, r_value, p_value, error = stats.linregress(BVs, mV)
+    MiddleData[index[axes[2]]] = BVs
+    MiddleData[index[axes[3]]] = mV
+    Middleindex[2] = index[axes[2]]
+    Middleindex[3] = index[axes[3]]
 
-    #Calc os standar deviation of the intercept
+    saveLaTex(MiddleData, Middleindex, [0,1,2,3], folderPath+'TexTables/middleData.tex')
+
+    # Calculate the linear regression
+    notslope, intercept, r_value, p_value, error = stats.linregress(BVs, mV)
+
+    # Calc of standar deviation of the slope
+    #   Due to the slope value is fixed by the absolute magnitude slope, the value given 
+    #   by the regression has been despised so its standard deviation has to be re-calculated
+    #   However the intercept value is used
+    recta = []
+    for i in xrange(len(BVs)):
+        recta.append((mV[i]-slope*BVs[i]-intercept)**2)
+    sigma = np.sqrt(sum(recta)/(len(BVs)-2))
+
+    BVs2 = [ i**2 for i in BVs ]
+    
+    error = (np.sqrt(len(BVs))*sigma/np.sqrt((len(BVs)*sum(BVs))-sum(BVs)**2))
+
+    #Calc of standar deviation of the intercept
     BVmean = np.mean(BVs)
     sx2 = ((BVs-BVmean)**2).sum()
     std_intercept = error * np.sqrt(1./len(BVs) + BVmean*BVmean/sx2)
@@ -150,7 +176,7 @@ def Plot2(data, index, axes, name):
     ax2.tick_params('y', colors='b')
 
     fig.tight_layout()
-    plt.show()
+    #plt.show()
 
     fig.set_size_inches(9,6)
     fig.savefig(folderPath+"Figures/"+name+".png") #save graph
@@ -172,8 +198,10 @@ Data['$m_B-m_V$'] = BlessV
 # add theorical color index for a given group of stars
 index[4] = '$M_V$'
 Data['$M_V$'] = MVgiven
-index[5] = '$[m(B)-m(V)]_{teo}$'
-Data['$[m(B)-m(V)]_{teo}$'] = BVgiven
+index[5] = '$M_B-M_V$'
+Data['$M_B-M_V$'] = BVgiven
+
+saveLaTex(Data, index, [4,5], folderPath+'TexTables/givenData.tex') #save the data in Latex table format
 
 # represent apparent visual magnitude and experimental B-V
 Plot(Data, index, [3, 2], 'expAparent', "A")
@@ -188,12 +216,6 @@ slopes, intercepts = Plot2(Data, index, [5, 4, 3, 2], 'both')
 fw.write("Slopes: \n \t m = %g +- %g" %(slopes["m"][0], slopes["m"][1]) 
              + "\n \t M = %g +- %g \n" %(slopes["M"][0], slopes["M"][1]))
 fw.write("Intercept: \n \t m = %g +- %g" %(intercepts["m"][0], intercepts["m"][1]) 
-            + "\n \t M = %g +- %g \n" %(intercepts["M"][0], intercepts["M"][1]))
-
-#print the results
-print ("Slopes: \n \t m = %g +- %g" %(slopes["m"][0], slopes["m"][1]) 
-             + "\n \t M = %g +- %g \n" %(slopes["M"][0], slopes["M"][1]))
-print ("Intercept: \n \t m = %g +- %g" %(intercepts["m"][0], intercepts["m"][1]) 
             + "\n \t M = %g +- %g \n" %(intercepts["M"][0], intercepts["M"][1]))
 
 ########################################
@@ -212,7 +234,6 @@ STD = ErrorsCalculator("m M",[intercepts["m"][0], intercepts["M"][0]],
 
 
 fw.write("M-m = %g +- %g \n" %(mM, STD)) # save the results
-print "M-m = %g +- %g \n" %(mM, STD) #print the results
 
 ##################################
 #     CALCUALTE THE DISTANCE    ##
@@ -224,7 +245,6 @@ D = 10**((mM/5)+1)
 STD_D = ErrorsCalculator("mM",[mM], [STD], "10**((mM/5)+1)")
 
 fw.write("D = %g +- %g pc \n" %(D, STD_D)) # save the results
-print "D = %g +- %g pc \n" %(D, STD_D) #print the results
 
 # in LY, 1pc = 3.26156 ly
 
@@ -232,6 +252,5 @@ Dly = D*3.26156
 std_Dly = STD_D*3.26156
 
 fw.write("D = %g +- %g ly \n" %(Dly, std_Dly)) # save the results
-print "D = %g +- %g ly \n" %(Dly, std_Dly) #print the results
 
 fw.close()
